f1fee4c2482c56ddeb0a28d6b7f52e9138ec9679
CierraM <4cibear@gmail.com>
2020-07-16 17:31:48 -0600

    debugging

--- tesla_app.py
+++ tesla_app.py
@@ -1,33 +1,20 @@
 import requests
 import sys
+import json
+import time
+
 
 
 BaseURL = "https://owner-api.teslamotors.com/"
 
-def test(): #try getting a response from the API
-
-    bearerKey = "dd5a5ffe7915426d5730c6f6223cc827939759a9bb5e0e14a82cb36e7d60b411" #input('Please input the bearer key.')
-
-    headers = {
-        "Authorization": f"Bearer {bearerKey}"
-    }
-    
-    
-    
-    getVehicle = "api/1/vehicles"
-    r = requests.get(BaseURL + getVehicle, headers=headers)
-    print(r.json())
-
-    id = r.json()["response"][0]["id"]
-    print(id)
-
-
 def main():
-    #Start Request. Make sure it goes through correctly. Error handling.
-    key = "dd5a5ffe7915426d5730c6f6223cc827939759a9bb5e0e14a82cb36e7d60b411" #input('Please input the bearer key.')
+    # read bearer key from a separate file.
+    f = open("bearerKey.txt", "r")
+    key = (f.read())
 
-    headers = {"Authorization": f"Bearer {key}"}
-
+    #set up headers
+    global headers
+    headers = {"Authorization": f"Bearer {key}", "Content-Type": "application/json"}
     
     getVehicle = "api/1/vehicles"
     
@@ -39,9 +26,10 @@ def main():
     except Exception as ex:
         print(ex)
 
+    global id
     id = r.json()["response"][0]["id"]
 
-    wake(id, headers)
+    wake()
 
     #choose between state and command. If command, choose which option. Put in a loop. Provide exit option also.
     root_choices = [display_state, commands, exit, "View Car State", "Execute a command", "exit"]
@@ -51,7 +39,7 @@ def main():
                 "[3] Exit"]
     choice = choose(choices)
     try:
-        root_choices[int(choice)-1](id, headers)
+        root_choices[int(choice)-1]()
     except TypeError: # This is to nullify the passing of too many parameters into exit()
         exit()
 
@@ -59,13 +47,13 @@ def main():
 # Lists off choices and returns the user's choice. Make sure it is a number. Confirm choice. Error handling.
 def choose(choices, *addReturn):
     print()
+    if addReturn:
+        print(f"Please choose (1-{len(choices) + 1}):")
+        choices.append(f'[{len(choices) + 1}] Return to home menu') # add return option
     while True:
         #print off options and get input
-        if addReturn:
-            print(f"Please choose (1-{len(choices) + 1}):")
-            choices.append(f'[{len(choices) + 1}] Return to home menu') # add return option
-        else:
-            print(f"Please choose (1-{len(choices)}):")
+        print()
+        print(f"Please choose (1-{len(choices)}):")
         i = 0
         while (i < len(choices)):
             print(choices[i])
@@ -93,13 +81,12 @@ def choose(choices, *addReturn):
 
     
 #wake the car up. Use a loop so further commands cannot be executed until car is awake.
-def wake(id, headers):
+def wake():
     print('Welcome to the Tesla Computer App.')
     print('Please wait a moment while your Tesla wakes up.')
     try:
         r = requests.post(f'{BaseURL}/api/1/vehicles/{id}/wake_up', headers=headers)
     except Exception as ex:
-        print(f'status code {r.status_code}')
         print(f'Exception in wake(): {ex}')
 
 
@@ -113,128 +100,261 @@ def wake(id, headers):
 
 
 # Choose a command. Return URL for it. 
-def commands(id, headers):
+def commands():
     commandURLs = [
         #0 "honk" : 
         f'/api/1/vehicles/{id}/command/honk_horn',
         #1 "lights" : 
         f'/api/1/vehicles/{id}/command/flash_lights',
-        #2 "toggleValetMode" : 
-        f'/api/1/vehicles/{id}/command/set_valet_mode',
-        #3 "unlockDoor" : 
+        #2 "unlockDoor" : 
         f'/api/1/vehicles/{id}/command/door_unlock',
-        #4 "lockDoor" : 
+        #3 "lockDoor" : 
         f'/api/1/vehicles/{id}/command/door_lock',
-        #5 "trunk" : 
-        f'/api/1/vehicles/{id}/command/actuate_trunk', # Note: Look at documentation to understand parameters.
-        #6 "ventWindows" : 
+        #4 "trunk" : 
+        f'/api/1/vehicles/{id}/command/actuate_trunk', # Can't get this to work, so it is not in use right now
+        #5 "ventWindows" : 
         f'/api/1/vehicles/{id}/command/window_control', #parameters required
-        #7 "openChargePort" : 
+        #6 "openChargePort" : 
         f'/api/1/vehicles/{id}/command/charge_port_door_open',
-        #8 "setChargeLimit" : 
+        #7 "setChargeLimit" : 
         f'/api/1/vehicles/{id}/command/set_charge_limit', #parameters required
-        #9 "setTemp" : 
+        #8 "setTemp" : 
         f'/api/1/vehicles/{id}/command/set_temps', #parameters required *Note: include conversion from fahr to cels for this.
-        #10 "toggleMedia" : 
-        f'/api/1/vehicles/{id}/command/media_toggle_playback',
-        #11 "nextTrack" : 
-        f'/api/1/vehicles/{id}/command/media_next_track',
-        #12 "previousTrack" : 
-        f'/api/1/vehicles/{id}/command/media_prev_track',
-        #13 "volUp" : 
-        f'/api/1/vehicles/{id}/command/media_volume_up',
-        #14 "volDown" : 
-        f'/api/1/vehicles/{id}/command/media_volume_down'
+        #9 turn on climate control
+        f'/api/1/vehicles/{id}/command/auto_conditioning_start',
+        #10 turn off climate control
+        f'/api/1/vehicles/{id}/command/auto_conditioning_stop',
+        #11 close charge port door
+        f'/api/1/vehicles/{id}/command/charge_port_door_close'
     ]
 
     commands = [
-        "[1] Honk horn",
+        "[1] Honk horn", 
         "[2] Flash lights",
-        "[3] Turn on/off valet mode",
-        "[4] Lock/unlock door",
-        "[5] Open trunk/frunk",
-        "[6] Vent windows",
-        "[7] Open charge port",
-        "[8] Set charge limit",
-        "[9] Set temperature",
-        "[10] See Media commands"
+        "[3] Lock/unlock door", 
+        "[4] Vent windows",
+        "[5] Open/close charge port",
+        "[6] Set charge limit",
+        "[7] Set temperature",
+        "[8] Turn on/off climate control" 
+        
     ]
 
-    media = [
-        "[1] Turn media on/off",
-        "[2] Skip track",
-        "[3] Previous track",
-        "[4] Volume up",
-        "[5] Volume down"
-    ]
+
     
-    command = choose(commands, True)
 
-    #Pull up media controls if media is selected
-    if command == "10":
-        command = choose(media, True) + 9
+    command = choose(commands, True)
+    
+    command = int(command)
+    # Shift numbers around so that command will line up with url index
+    if command == 1:
+        urlCommand = 0
+
+    elif command == 2:
+        urlCommand = 1
+
+    elif command == 3: #Is it locked?
+        if check_state(command) == True:
+            urlCommand = 2 #unlock door
+        else:
+            urlCommand = 3 #lock door
+
+    elif command == 4:
+        urlCommand = 5
+
+    elif command == 5:
+        if check_state(command) == True:
+            urlCommand = 11 #close door
+        else:
+            urlCommand = 6 #open door
+
+    elif command == 6:
+        urlCommand = 7
+    
+    elif command == 7:
+        urlCommand = 8
+
+    elif command == 8:
+        urlCommand = 8
+
+    elif command == 8: #Is climate on?
+        if check_state(command) == True:
+            urlCommand = 10 # turn off climate
+        else: 
+            urlCommand = 9 # turn on climate
+
+    else:
+        print("There was some problem and the command could not be found.")
+        main()
+
 
     #Choose the correct url from the list, depending on what the user selected.
-    url = commandURLs[int(command)-1]
+    url = commandURLs[int(urlCommand)]
+
     
     #set parameters for the options that require parameters
 
-    if command == 5:
-        choices = [
-        "[1] Open trunk",
-        "[2] Open frunk"
-        ]
-        choice = choose(choices, True)
-        if choice == 1:
-            params = {"which_trunk":"trunk"}
-        elif choice == 2:
-            params = {"which_trunk":"frunk"}
+    if command == 4:
+        params = {"lat": "0", "lon": "0", "command": "vent"}
 
     elif command == 6:
-        params = {"command":"vent"}
+        while True:
+            choice = int(input("Please set the charge limit (50-100)%"))
+            if choice > 50 and choice <= 100:
+                params = {"percent":choice}
+                break
+            else:
+                print("Please choose a number between 50 and 100")
 
-    elif command == 8:
-        choice = input("Please set the charge limit (50-100)%")
-        params = {"percent":choice}
-    elif command == 9:
+    elif command == 7:
         choice = fahr_to_cels(input("What would you like to set the temperature to?"))
+        
         params = {"driver_temp":choice,"passenger_temp":choice}
 
     #If params are required, use them, if not, don't.
     try:
-        executeCommand(url, headers, params)
+        
+        
+        executeCommand(url, command, params)
+        
     except UnboundLocalError:
-        executeCommand(url, headers, command)
-
-
+        executeCommand(url, command)
+    
+    
+    print()
+    returnToMain()
     
 
 # function for executing commands.
-def executeCommand(url, headers, *params, command):
+def executeCommand(url, command, *params):
 
+    try:                #Just making sure if no parameters are passed in, the function still works.
+        parameters = json.dumps(params[0])
+    except:
+        parameters = ""
     try:
-        r = requests.post(f'{BaseURL}{url}', headers=headers, params=params)
+        r = requests.post(f'{BaseURL}{url}', headers=headers, data=parameters)
         
         if r.status_code == 200:
-            print("Success")
+            jsonData = r.json()
+            response = (jsonData["response"]["result"])
+            if response:
+                print("Success")
+                print_command_status(command)
+            else:
+                print("Command Unsuccesful")
 
     except Exception as ex:
         print(f'Error: {ex}')
-        print("Command Unsuccesful")
-        main()
+        returnToMain()
+    
+#This function is to check where something is at before deciding which command to use.
+def check_state(command):
+    url= f'{BaseURL}/api/1/vehicles/{id}/vehicle_data'
 
-    print_command_status(command)
+    if command == 3: #check is door locked
+        url= f'{BaseURL}/api/1/vehicles/{id}/vehicle_data'
+        try: 
+            r = requests.get(url, headers=headers)
+            data = r.json()
+            if data["response"]["vehicle_state"]["locked"]:
+                return True
+            else:
+                return False
+        except Exception as ex:
+            print(ex)
+            
+    elif command == 5:
+        url= f'{BaseURL}/api/1/vehicles/{id}/vehicle_data'
+        try: 
+            r = requests.get(url, headers=headers)
+            data = r.json()
+            if data["response"]["charge_state"]["charge_port_door_open"]:
+                return True
+            else:
+                return False
+        except Exception as ex:
+            print(ex)
+            
+
+    elif command == 8: # check is climate control on
+        
+        url= f'{BaseURL}/api/1/vehicles/{id}/data_request/climate_state'
+        try: 
+            r = requests.get(url, headers=headers)
+            data = r.json()
+            if data["response"]["is_climate_on"]:
+                return True
+            else:
+                return False
+        except Exception as ex:
+            print(ex)
+        
+    else:
+        print("check_state received the wrong number")
+        return
 
 
 #This function will take the executed command and relay back pertinent info to the user.
+#command is a number 1-8 correlating to one of the commands. 
 def print_command_status(command):
-    statusDict = {
+    url= f'{BaseURL}/api/1/vehicles/{id}/vehicle_data'
+    for second in range(2): # This is to give the program some time to update before printing message
+        command = int(command)
+        time.sleep(1)   
+        try: 
+            r = requests.get(url, headers=headers)
+            data = r.json()
+        except Exception as ex:
+            print(ex)    
+        second += 1
+
+    if command == 1: #honk
+        message = "The horn has been honked."
+
+    elif command == 2: #lights
+        message = "The lights have been flashed"
+
+    elif command == 3:
         
-    }
+        if check_state(3):
+            message = "The car is locked."
+        else:
+            message = "The car is unlocked."
+
+    elif command == 4:#vent windows
+        if data["response"]["vehicle_state"]["fd_window"] == 0:
+            message = "The windows are closed."
+        else:
+            message = "The windows are vented or open."
+
+    elif command == 5:#open charge port
+        if data["response"]["charge_state"]["charge_port_door_open"]:
+            message = "The charge port door is open."
+        else:
+            message = "The charge port door is closed."
+    elif command == 6:#set charge limit
+        message = f'The charge limit is set to {data["response"]["charge_state"]["charge_limit_soc"]}%.'
+
+    elif command == 7:#set temp  
+        message = f'The climate control is set to {cels_to_fahr(data["response"]["climate_state"]["driver_temp_setting"])}.\nThe current temperature inside the car is {cels_to_fahr(data["response"]["climate_state"]["inside_temp"])}.'
+
+    elif command == 8:#turn on climate control
+        if data["response"]["climate_state"]["is_climate_on"]:
+            message = "Climate control is on."
+        else:
+            message = "Climate control is off"
+    else:
+        message = "Unable to display confirmation message."
+        
+    print(message)
+
+
+
 
 
 # display car state information
-def display_state(id, headers):
+def display_state():
 
     # Code to make it do things:
     # Put the url extensions in a list with all the things I want it to do.
@@ -256,22 +376,31 @@ def display_state(id, headers):
         "Charge State": r.json()["response"]["charge_state"]["charging_state"]
     }
     cleanPrint("Car State:", data)
+    leave = input("Return to main menu? (y/n)")
+    if leave.lower() == "n":
+        exit()
+    else:
+        main()
 
 
-
-
+def returnToMain():
+    leave = input("Return to commands? (y/n)")
+    if leave.lower() == "n":
+        exit()
+    else:
+        commands()
+    
 #Prints a dictionary in a user friendly way, leaving some space before and after
 def cleanPrint(header, dict):
     print()
     print(header)
-    for key, value in dict.items():
-        
+    for key, value in dict.items():    
         print("{}: {}".format(key, value))
     print()
 
 #converts celsius to fahrenheit, adds degrees sign
 def cels_to_fahr(cels):
-    fahr = round((float(cels) * 1.8 + 32), 1)
+    fahr = round((float(cels) * (9/5) + 32), 1)
     fahr = str(fahr) + "°F"
     return fahr
 
@@ -281,7 +410,7 @@ def fahr_to_cels(fahr):
         if not fahr[char].isdigit():
             fahr.replace(fahr[char], "")
 
-    cels = round((float(fahr) -32) * (5/9))
+    cels = (float(fahr) -32) * (5/9)
     return cels
 
 main()
--- test_tesla_app.py
+++ test_tesla_app.py
@@ -1,10 +1,24 @@
 import pytest
-from pytest import approx
+from pytest import approx 
 from tesla_app import cels_to_fahr
 from tesla_app import fahr_to_cels
 
 
 def test_cels_to_fahr():
-
-    
+    assert cels_to_fahr(75) == "167°F"
+    assert cels_to_fahr(5) == "41°F"
+    assert cels_to_fahr(32) == "89.6°F"
+    assert cels_to_fahr(-2) == "28.4°F"
+    assert cels_to_fahr(40) == "104°F"
+    assert cels_to_fahr(22) == "71.6°F"
+    1
 def test_fahr_to_cels():
+    assert fahr_to_cels(75) == pytest.approx(23.8889, .01)
+    assert fahr_to_cels("75°F") == pytest.approx(23.8889, .01)
+    assert fahr_to_cels("60") == pytest.approx(15.5556)
+    assert fahr_to_cels(90) == pytest.approx(32.2222)
+    assert fahr_to_cels("7m9h") == pytest.approx(26.1111)
+    assert fahr_to_cels(105) == pytest.approx(40.5556)
+    assert fahr_to_cels(-7) == pytest.approx(-21.6667)
+
+pytest.main(["test_tesla_app.py"])
